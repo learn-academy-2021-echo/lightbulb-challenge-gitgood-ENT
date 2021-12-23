e5954fe040de3b6aff1df933f0db034d9d591324
gitgood-ENT <ward.noahd@gmail.com>
2021-12-22 23:36:08 -0800

    working while  on a plane

--- src/App.js
+++ src/App.js
@@ -2,9 +2,35 @@ import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      status: false
+    }
+  }
+
+  toggle = () => {
+    this.setState(this.status === true ? this.status = false : this.status = true)
+  }
+
+  
+
   render(){
+
+    let switchStyles = {
+      border: "2px solid black",
+      width: "70px",
+      height: "140px",
+      margin: "10px",
+      padding: "10px"
+    }
+
+
     return(
       <>
+      <button style={switchStyles} onClick={this.toggle}>
+        on/off
+      </button>
         <h1>Hello World!</h1>
       </>
     )
