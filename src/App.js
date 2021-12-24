53d01299657e090c586d568f869a1d2b8fa43c9b
gitgood-ENT <ward.noahd@gmail.com>
2021-12-23 23:16:44 -0500

    added a header

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ class App extends Component{
   render(){
     return(
       <>
+      <h1>Lightbulbs?!</h1>
       <button id='onOff' style={{backgroundColor: this.state.lightColor}} onClick={this.toggle}>
         {this.state.status}
       </button>
