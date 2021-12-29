e6925b81ab999ddd7e0a84e849888c8f93d06eaf
gitgood-ENT <ward.noahd@gmail.com>
2021-12-28 17:22:26 -0800

    added functionality to click lightswitch image

---
+++ src/components/LightSwitch.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import  BulbOff  from "./svg/bulb-off.svg";
+import  BulbOn  from "./svg/bulb-on.svg";
+import KnifeOff from "./svg/knifeOff.svg";
+import KnifeOn from "./svg/knifeOn.svg";
+
+// In order to get the image to be a button I had to change the state to only include the path to the image and not img src, so that I could use the tags properly in the button tag below.
+
+class LightSwitch extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      status: 'off',
+      light: <img src={BulbOff}/>,
+      switch: KnifeOff,
+    }
+  }
+
+  lightToggle = () => {
+    this.setState({
+      status:
+        this.state.status === 'off' ? // if it is off
+          ('on') : // turn it on
+          ('off'), // else turn it off
+      light:
+        this.state.status === 'on' ? // same logic as above but controlling color
+          (<img src={BulbOff}/>) :
+          (<img src={BulbOn}/>),
+      switch:
+        this.state.status === 'on' ? // same logic again
+          (KnifeOff) :
+          (KnifeOn)
+        });
+  };
+
+ 
+
+
+
+  render(){
+    return(
+      <>
+      <div id='cent'>
+
+      <button id='switch'><img src={this.state.switch} onClick={this.lightToggle}/></button>
+      
+      {this.state.light}
+      </div>
+      </>
+    )
+  }
+}
+export default LightSwitch
