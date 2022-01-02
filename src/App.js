f847f02b7572bb4351432252e67ff8cadb818865
gitgood-ENT <ward.noahd@gmail.com>
2022-01-02 14:32:48 -0800

    added add/remove buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
-import LightBox from './components/LightBox'
-import LightSwitch from "./components/LightSwitch";
+import AddRemove from "./components/AddRemove";
 
 
 
@@ -9,9 +8,10 @@ export default class App extends Component {
   render() {
     return (
       <>
-      <h1>Lightbulbs?!</h1>
+      <h1 id='header'>Lightbulbs?!</h1>
+      
       <div id='lights'>
-        <LightSwitch />
+        <AddRemove />   
        
         
       </div>
